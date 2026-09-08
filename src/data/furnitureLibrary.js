4cf0ead8d2658e092ac286c80af52c69bce329c5
Webcreator-tech <creatorconnectin04@gmail.com>
2026-09-08 21:06:58 +0530

    Update furnitureLibrary.js

--- src/data/furnitureLibrary.js
+++ src/data/furnitureLibrary.js
@@ -10,7 +10,7 @@ export const FURNITURE_LIBRARY = [
     id: "tv",
     name: "TV",
     modelPath: "/models/furniture/wall_flat_tv.glb",
-    defaultScale: [0.00015, 0.00015, 0.00015],
+    defaultScale: [0.0015, 0.0015, 0.0015],
   },
 ];
 

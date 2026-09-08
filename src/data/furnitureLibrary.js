4998746dc0130b28adb74163bd7e32d30035692a
Webcreator-tech <creatorconnectin04@gmail.com>
2026-09-08 20:51:56 +0530

    Update furnitureLibrary.js

--- src/data/furnitureLibrary.js
+++ src/data/furnitureLibrary.js
@@ -3,7 +3,7 @@ export const FURNITURE_LIBRARY = [
     id: "sofa",
     name: "Sofa",
     modelPath: "/models/furniture/sofa_02.glb",
-    defaultScale: [0.0015, 0.0015, 0.0015],
+    defaultScale: [0.00015, 0.00015, 0.00015],
   },
 
   {
